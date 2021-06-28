1d533ce8f15f4e05afa0bbbf4a073fb240dbd627
IMRANDIL <aliimranadil2@gmail.com>
2021-06-28 09:49:21 +0530

    Updates

---
+++ precache-manifest.d760d637087968e48c592c260bfc027a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1e7ec13ef43e2276c35b4ccc7539fe0",
+    "url": "/stripe-submenu-react/index.html"
+  },
+  {
+    "revision": "d9796eda5c449e459252",
+    "url": "/stripe-submenu-react/static/css/main.990a8c41.chunk.css"
+  },
+  {
+    "revision": "9d9ce4078b442a71cc60",
+    "url": "/stripe-submenu-react/static/js/2.e1e77988.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stripe-submenu-react/static/js/2.e1e77988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9796eda5c449e459252",
+    "url": "/stripe-submenu-react/static/js/main.33448df8.chunk.js"
+  },
+  {
+    "revision": "21714b126ef5a48f5304",
+    "url": "/stripe-submenu-react/static/js/runtime-main.627c99b9.js"
+  },
+  {
+    "revision": "8596ab4a635cf6b6ea535b15bdc0dc67",
+    "url": "/stripe-submenu-react/static/media/hero.8596ab4a.svg"
+  },
+  {
+    "revision": "1090473dd728fcc17033df70118fa668",
+    "url": "/stripe-submenu-react/static/media/logo.1090473d.svg"
+  },
+  {
+    "revision": "58d7e3d689c0cb8eb365b86046ecdce8",
+    "url": "/stripe-submenu-react/static/media/phone.58d7e3d6.svg"
+  }
+]);
